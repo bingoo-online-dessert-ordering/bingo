eb0e5a3935b50344cf40ca9da9d8b5a9cb2dde1c
miloni22 <milonishah1999@gmail.com>
2019-11-12 23:20:01 +0530

    login + extra tab for owner

--- cakes.php
+++ cakes.php
@@ -1,3 +1,21 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['email'])) {
+        header('Location: http://localhost:8000/bingo/index.php');  
+                    
+    }
+    else {
+        $now = time(); // Checking the time now when home page starts.
+
+        if ($now > $_SESSION['expire']) {
+            session_destroy();
+        header('Location: http://localhost:8000/bingo/index.php');  
+        }
+        else { //Starting this else one [else1]
+?>
+
+
 <!DOCTYPE html>
 <html>
 
@@ -17,11 +35,22 @@
 
   <div id="navbar">
     <ul>
-      <li><a class="active" href="index.php">Home</a></li>
+      <li><a class="active" href="home.php">Home</a></li>
       <li><a href="cakes.php" style="background-color: white; color: grey">Cakes</a></li>
       <li><a href="icecream.php">Ice Creams</a></li>
       <li><a href="cheesecakes.php">Cheesecakes</a></li>
+      <?php
+
+      if($_SESSION['type']==1){
+        ?>
+        <li><a href="eatery.php">My Eatery</a></li>
+      <?php 
+      }else{ ?>
+
       <li><a href="joinus.php">Join Us</a></li>
+     <?php }
+
+      ?>
     </ul>
   </div>
 <br>
@@ -39,19 +68,19 @@
   if(!$conn)
   echo "Connection error : " .mysqli_connect_error();
 
-  $retrieving_data = "select * from eatery_details order by e_rating DESC";
+  $retrieving_data = "select * from eatery where type='cakes' order by rating DESC";
   $result = $conn->query($retrieving_data);
     while($row = $result->fetch_assoc()) { ?>
       <div id="list">
       <img id="img<?= $i ?>" src="./images/cake<?= $i ?>.jpg" alt="cake" class="rep_img" >
-      <font size="3" id="anchor2"><b><?= $row["e_name"]?></b></font><br>
+      <font size="3" id="anchor2"><b><?= $row["name"]?></b></font><br>
       <?php
-      for($j=0; $j < $row["e_rating"]; $j++) { ?>
+      for($j=0; $j < $row["rating"]; $j++) { ?>
         <img src="./images/star.jpg" alt="star" class="star" >
       <?php } ?>
       <br>
-      <font size="2"><?= $row["e_address"]?></font><br>
-      <font size="2" ><?= $row["e_cost"]?> for 2</font>
+      <font size="2"><?= $row["address"]?></font><br>
+      <font size="2" ><?= $row["cost"]?> for 2</font>
 
 
       </div>
@@ -66,3 +95,6 @@ $i=1;
 
 </body>
 </html>
+<?php 
+}
+} ?>
